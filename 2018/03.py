8b4f33ee9de26b3b5a1ba7f95c3e316614bc4f66
petertrr <peter.trifanov@mail.ru>
2018-12-03 21:27:09 +0300

    Dec 3 2018 solution

---
+++ 2018/03.py
@@ -0,0 +1,47 @@
+import re
+from itertools import chain
+from functools import reduce
+
+example = \
+    '''#1 @ 1,3: 4x4
+    #2 @ 3,1: 4x4
+    #3 @ 5,5: 2x2'''
+
+task = open("03.input").readlines()
+
+
+class Claim:
+    pattern = r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)'
+
+    def __init__(self, claim_str):
+        arr = re.findall(self.pattern, claim_str)[0]
+        self.id = int(arr[0])
+        self.margin_x = int(arr[1])
+        self.margin_y = int(arr[2])
+        self.width_x = int(arr[3])
+        self.width_y = int(arr[4])
+
+
+claims = set(Claim(line.strip()) for line in task)
+
+x_max = max(c.margin_x + c.width_x for c in claims)
+y_max = max(c.margin_y + c.width_y for c in claims)
+
+fabric = [[0 for _ in range(y_max)] for _ in range(x_max)]
+
+overlaps = {c.id: False for c in claims}
+overlaps.update({-1: True})
+
+for c in claims:
+    for i in range(c.margin_x, c.margin_x + c.width_x):
+        for j in range(c.margin_y, c.margin_y + c.width_y):
+            if fabric[i][j] == 0:
+                fabric[i][j] = c.id
+            elif fabric[i][j] != 0:
+                overlaps[fabric[i][j]] = True
+                overlaps[c.id] = True
+                fabric[i][j] = -1
+
+print(reduce(lambda count, i: count + (1 if i < 0 else 0), chain(*fabric)))
+
+print(next(c for c in overlaps if not overlaps[c]))
