e5c606452154865975bb8060d780d4639f297292
petertrr <peter.trifanov@mail.ru>
2018-12-05 21:48:44 +0300

    Applied stack solution

--- 2018/05.py
+++ 2018/05.py
@@ -1,22 +1,20 @@
 task = open('05.input').readlines()[0].strip()
 
+
 def react(polymer):
-    i = 0
-    while i < len(polymer):
-        if i < len(polymer) - 1 and polymer[i].swapcase() == polymer[i+1]:
-            polymer = polymer[:i] + polymer[i+2:]
-            i = max(0, i - 1)
+    stack = []
+    for s in polymer:
+        if stack and stack[-1].swapcase() == s:
+            stack.pop()
         else:
-            i += 1
-    return polymer
+            stack.append(s)
+    return ''.join(stack)
+
 
 result = react(task)
 print(len(result))
 
-lens = {}
-for letter in map(chr, range(ord('a'), ord('z') + 1)):
-    cur = ''.join(s for s in task if s.lower() != letter)
-    lens.update({letter: len(react(cur))})
-
+lens = {letter: len(react(''.join(s for s in task if s.lower() != letter)))
+        for letter in map(chr, range(ord('a'), ord('z') + 1))}
 min_letter = min(lens, key=lambda key: lens[key])
 print(min_letter, lens[min_letter])
