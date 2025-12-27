9fb808849aebd66394afc04ebed2f572ad50fc26
Peter Trifanov <peter.trifanov@gmail.com>
2025-12-27 20:10:02 +0100

    2025 Day 3 part 2

--- 2025/Aoc03.hs
+++ 2025/Aoc03.hs
@@ -12,14 +12,20 @@ maximumWithIndex :: Ord a => [a] -> (a, Int)
 -- reverse is needed, because maximumBy uses foldr
 maximumWithIndex list = maximumBy (comparing fst) $ reverse (zip list [0..])
 
-getJoltage :: [Int] -> Int
-getJoltage nums = n1 * 10 + n2
+getJoltage2 :: [Int] -> Int
+getJoltage2 nums = n1 * 10 + n2
     where (n1, idx1) = maximumWithIndex $ init nums
           n2 = maximum $ drop (idx1 + 1) nums
 
+getJoltageK k nums
+    | k == 2 = getJoltage2 nums
+    | otherwise = n1 * 10^(k-1) + n2
+    where (n1, idx1) = maximumWithIndex $ take (length nums - k + 1) nums
+          n2 = getJoltageK (k-1) $ drop (idx1 + 1) nums
+          
 aoc2025_03 args = do
   content <- readFile $ head args
   let linesOfFile = lines content
   print . show . getResult $ linesOfFile
-    where getResult linesOfFile = sum $ map (getJoltage . inputNumbers) linesOfFile
+    where getResult linesOfFile = sum $ map ((getJoltageK 12) . inputNumbers) linesOfFile
           inputNumbers = map digitToInt
